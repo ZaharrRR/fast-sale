2ed5f04b5464d1ff03ac351e7fb8cccb4f51c228
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2024-07-17 19:23:07 +0500

    account page init

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,5 +1,57 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>account</main>
+  <div class="account">
+    <header class="account-head">
+      <div class="head-account">
+        <div class="account-user">
+          <img src="/icons/account.svg" />
+          <span>username</span>
+        </div>
+
+        <button>Выйти</button>
+      </div>
+    </header>
+
+    <main class="account-main">
+      <div class="block">
+        <span>3342 ₽</span>
+
+        <button>Пополнить</button>
+      </div>
+
+      <div class="block"></div>
+    </main>
+  </div>
 </template>
+
+<style scoped>
+.account {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.account-head {
+  border: 1px solid black;
+
+  border-radius: 0 0 14px 14px;
+  padding: 12px;
+}
+
+.head-account {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.account-user {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.account-main {
+  padding: 12px;
+}
+</style>
